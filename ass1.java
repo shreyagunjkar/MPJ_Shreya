b4675525aee9c6e778ae3c38be50060b7d99ba41
shreyagunjkar55-beep <shreyagunjkar55@gmail.com>
2026-02-17 08:44:25 +0530

    Update ass1.java

--- ass1.java
+++ ass1.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 class Student {
     private String name;
     private int rollNo;
-    private int[] marks = new int[5];
+    private int mark1, mark2, mark3, mark4, mark5;
 
-    public Student(String name, int rollNo, int[] marks) {
+    public Student(String name, int rollNo, int mark1, int mark2, int mark3, int mark4, int mark5) {
         this.name = name;
         this.rollNo = rollNo;
-        this.marks = marks;
+        this.mark1 = mark1;
+        this.mark2 = mark2;
+        this.mark3 = mark3;
+        this.mark4 = mark4;
+        this.mark5 = mark5;
     }
 
     public double calculateAverage() {
-        int sum = 0;
-        for (int m : marks) {
-            sum += m;
-        }
+        int sum = mark1 + mark2 + mark3 + mark4 + mark5;
         return sum / 5.0;
     }
 
@@ -48,14 +49,16 @@ public class ass1 {
         System.out.print("Enter Roll Number: ");
         int rollNo = sc.nextInt();
 
-        int[] marks = new int[5];
         System.out.println("Enter marks for 5 subjects:");
-        for (int i = 0; i < 5; i++) {
-            marks[i] = sc.nextInt();
-        }
+        int m1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int m3 = sc.nextInt();
+        int m4 = sc.nextInt();
+        int m5 = sc.nextInt();
 
-        Student s = new Student(name, rollNo, marks);
+        Student s = new Student(name, rollNo, m1, m2, m3, m4, m5);
         s.display();
+
         sc.close();
     }
 }
